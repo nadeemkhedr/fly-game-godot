00b6a57b55361360e255fbd92b0b9c94d4459018
Nadeem Khedr <nadeemkhedr@gmail.com>
2023-09-24 19:02:28 +0300

    ✨ feat: add parallax background

---
+++ scrolling_bg/scrolling_bg.gd
@@ -0,0 +1,16 @@
+extends ParallaxBackground
+
+
+const SPEED = 120.0
+
+
+func _ready():
+	GameManager.on_game_over.connect(on_game_over)
+
+
+func _process(delta):
+	scroll_offset.x += SPEED * delta * -1
+	
+
+func on_game_over():
+	set_process(false)
